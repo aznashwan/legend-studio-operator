93cf6a40a51d0f84badc8fe842f33bbfa9df443a
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2021-09-13 19:54:21 +0300

    Initial charm draft.

--- src/charm.py
+++ src/charm.py
@@ -1,104 +1,405 @@
 #!/usr/bin/env python3
 # Copyright 2021 Canonical
 # See LICENSE file for licensing details.
-#
-# Learn more at: https://juju.is/docs/sdk
 
-"""Charm the service.
+""" Module defining the Charmed operator for the FINOS Legend Studio. """
 
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
-
+import functools
+import json
 import logging
 
-from ops.charm import CharmBase
-from ops.framework import StoredState
-from ops.main import main
-from ops.model import ActiveStatus
+from ops import charm
+from ops import framework
+from ops import main
+from ops import model
 
-logger = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
-class LegendEngineServerOperatorCharm(CharmBase):
-    """Charm the service."""
+STUDIO_UI_CONFIG_FILE_CONTAINER_LOCAL_PATH = "/ui-config.json"
+STUDIO_HTTP_CONFIG_FILE_CONTAINER_LOCAL_PATH = "/http-config.json"
 
-    _stored = StoredState()
+APPLICATION_CONNECTOR_TYPE_HTTP = "http"
+APPLICATION_CONNECTOR_TYPE_HTTPS = "https"
+
+VALID_APPLICATION_LOG_LEVEL_SETTINGS = [
+    "INFO", "WARN", "DEBUG", "TRACE", "OFF"]
+
+GITLAB_PROJECT_VISIBILITY_PUBLIC = "public"
+GITLAB_PROJECT_VISIBILITY_PRIVATE = "private"
+GITLAB_REQUIRED_SCOPES = ["openid", "profile", "api"]
+GITLAB_OPENID_DISCOVERY_URL = (
+    "https://gitlab.com/.well-known/openid-configuration")
+
+
+def _logged_charm_entry_point(fun):
+    """ Add logging for method call/exits. """
+    @functools.wraps(fun)
+    def _inner(*args, **kwargs):
+        LOG.info(
+            "### Initiating Legend Studio charm call to '%s'", fun.__name__)
+        res = fun(*args, **kwargs)
+        LOG.info(
+            "### Completed Legend Studio charm call to '%s'", fun.__name__)
+        return res
+    return _inner
+
+
+class LegendStudioServerOperatorCharm(charm.CharmBase):
+    """ Charmed operator for the FINOS Legend Studio. """
+
+    _stored = framework.StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+        self._set_stored_defaults()
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+        # Standard charm lifecycle events:
+        self.framework.observe(
+            self.on.config_changed, self._on_config_changed)
+        self.framework.observe(
+            self.on.studio_pebble_ready, self._on_studio_pebble_ready)
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
+        # DB relation lifecycle events:
+        self.framework.observe(
+            self.on["legend-db"].relation_joined,
+            self._on_db_relation_joined)
+        self.framework.observe(
+            self.on["legend-db"].relation_changed,
+            self._on_db_relation_changed)
+
+        # TODO(aznashwan): add SDLC and Engine relations:
+
+    def _set_stored_defaults(self) -> None:
+        self._stored.set_default(log_level="DEBUG")
+        self._stored.set_default(mongodb_credentials={})
+
+    @_logged_charm_entry_point
+    def _on_studio_pebble_ready(self, event: framework.EventBase) -> None:
+        """Define the Studio workload using the Pebble API.
+        Note that this will *not* start the service, but instead leave it in a
+        blocked state until the relevant relations required for it are added.
         """
         # Get a reference the container attribute on the PebbleReadyEvent
         container = event.workload
+
         # Define an initial Pebble layer configuration
         pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+            "summary": "Studio layer.",
+            "description": "Pebble config layer for FINOS Legend Studio.",
             "services": {
-                "httpbin": {
+                "studio": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
+                    "summary": "studio",
+                    "command": (
+                        # NOTE(aznashwan): starting through bash is required
+                        # for the classpath glob (-cp ...) to be expanded:
+                        "/bin/sh -c 'java -XX:+ExitOnOutOfMemoryError -Xss4M "
+                        "-XX:MaxRAMPercentage=60 -Dfile.encoding=UTF8 -cp "
+                        "/app/bin/webapp-content:/app/bin/* "
+                        "org.finos.legend.server.shared.staticserver.Server "
+                        "server %s" % (
+                            STUDIO_HTTP_CONFIG_FILE_CONTAINER_LOCAL_PATH)
+                    ),
+                    # NOTE(aznashwan): considering the Studio service expects
+                    # a singular config file which already contains all
+                    # relevant options in it (some of which will require the
+                    # relation with DB/Gitlab to have already been
+                    # established), we do not auto-start:
+                    "startup": "disabled",
+                    # TODO(aznashwan): determine any env vars we could pass
+                    # (most notably, things like the RAM percentage etc...)
+                    "environment": {},
                 }
             },
         }
+
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+        container.add_layer("studio", pebble_layer, combine=True)
 
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        # NOTE(aznashwan): as mentioned above, we will *not* be auto-starting
+        # the service until the relations with DBMan and Gitlab are added:
+        # container.autostart()
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+        self.unit.status = model.BlockedStatus(
+            "Awaiting Legend Database and Gitlab relations.")
 
-        Learn more about config at https://juju.is/docs/sdk/config
+    def _get_logging_level_from_config(self, option_name) -> str:
+        """Fetches the config option with the given name and checks to
+        ensure that it is a valid `java.utils.logging` log level.
+
+        Returns None if an option is invalid.
         """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+        value = self.model.config[option_name]
+        if value not in VALID_APPLICATION_LOG_LEVEL_SETTINGS:
+            LOG.warning(
+                "Invalid Java logging level value provided for option "
+                "'%s': '%s'. Valid Java logging levels are: %s. The charm "
+                "shall block until a proper value is set.",
+                option_name, value, VALID_APPLICATION_LOG_LEVEL_SETTINGS)
+            return None
+        return value
 
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
+    def _get_ui_config_from_relation_data(self):
+        """Returns a dict which can be serialized into JSON and passed to
+        the Studio service.
         """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
-        else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+        # TODO(aznashwan): fill in the URLs from relation data:
+        return {
+            "appName": "studio",
+            "env": "test",
+            "sdlc": {
+                "url": "http://10.107.9.20:7070/api"
+            },
+            "metadata": {
+                "url": "__LEGEND_DEPOT_URL__/api"
+            },
+            "engine": {
+                "url": "http://10.107.9.20:6060/api"
+            },
+            "documentation": {
+                "url": "https://legend.finos.org"
+            },
+            "options": {
+                "core": {
+                    # TODO(aznashwan): could this error in the future?
+                    "TEMPORARY__disableServiceRegistration": True
+                }
+            }
+        }
+
+    def _add_base_service_config_from_charm_config(
+            self, studio_http_config: dict = {}) -> model.BlockedStatus:
+        """This method adds all relevant Studio config options into the
+        provided dict to be directly rendered to JSON and passed to the Studio.
+
+        Returns:
+            None if all of the config options derived from the config/relations
+            are present and have passed Charm-side valiation steps.
+            A `model.BlockedStatus` instance with a relevant message otherwise.
+        """
+        # Check gitlab-related options:
+        # TODO(aznashwan): remove this check on eventual Gitlab relation:
+        gitlab_client_id = self.model.config.get('gitlab-client-id')
+        gitlab_client_secret = self.model.config.get('gitlab-client-secret')
+        if not all([
+                gitlab_client_id, gitlab_client_secret]):
+            return model.BlockedStatus(
+                "One or more Gitlab-related charm configuration options "
+                "are missing.")
+
+        # Check Java logging options:
+        pac4j_logging_level = self._get_logging_level_from_config(
+            "server-pac4j-logging-level")
+        server_logging_level = self._get_logging_level_from_config(
+            "server-logging-level")
+        if not all([
+                server_logging_level, pac4j_logging_level]):
+            return model.BlockedStatus(
+                "One or more logging config options are improperly formatted "
+                "or missing. Please review the debug-log for more details.")
+
+        # Check Mongo-related options:
+        mongo_creds = self._stored.mongodb_credentials
+        if not mongo_creds or 'replica_set_uri' not in mongo_creds:
+            return model.BlockedStatus(
+                "No stored MongoDB credentials were found yet. Please "
+                "ensure the Charm is properly related to MongoDB.")
+        mongo_replica_set_uri = self._stored.mongodb_credentials[
+            'replica_set_uri']
+        databases = mongo_creds.get('databases')
+        database_name = None
+        if databases:
+            database_name = databases[0]
+            # NOTE(aznashwan): the Java MongoDB can't handle DB names in the
+            # URL, so we need to trim that part and pass the database name
+            # as a separate parameter within the config as the
+            # studio_config['pac4j']['mongoDb'] option below.
+            split_uri = [
+                elem
+                for elem in mongo_replica_set_uri.split('/')[:-1]
+                # NOTE: filter any empty strings resulting from double-slashes:
+                if elem]
+            # NOTE: schema prefix needs two slashes added back:
+            mongo_replica_set_uri = "%s//%s" % (
+                split_uri[0], "/".join(split_uri[1:]))
+        studio_ui_path = self.model.config["server-ui-path"]
+
+        # Compile base config:
+        studio_http_config.update({
+            "uiPath": studio_ui_path,
+            "html5Router": True,
+            "server": {
+              "type": "simple",
+              "applicationContextPath": "/",
+              "adminContextPath": "%s/admin" % studio_ui_path,
+              "connector": {
+                "type": "APPLICATION_CONNECTOR_TYPE_HTTP",
+                "port": self.model.config[
+                    'server-application-connector-port-http']
+              }
+            },
+            "logging": {
+              "level": server_logging_level,
+              "loggers": {
+                "root": {"level": server_logging_level},
+                "org.pac4j": {"level": pac4j_logging_level}
+              },
+              "appenders": [{"type": "console"}]
+            },
+            "pac4j": {
+                "callbackPrefix": "/studio/log.in",
+                "bypassPaths": ["/studio/admin/healthcheck"],
+                "mongoUri": mongo_replica_set_uri,
+                "mongoDb": database_name,
+                "clients": [{
+                    "org.finos.legend.server.pac4j.gitlab.GitlabClient": {
+                        "name": "gitlab",
+                        "clientId": gitlab_client_id,
+                        "secret": gitlab_client_secret,
+                        "discoveryUri": GITLAB_OPENID_DISCOVERY_URL,
+                        # NOTE(aznashwan): needs to be a space-separated str:
+                        "scope": " ".join(GITLAB_REQUIRED_SCOPES)
+                    }
+                }],
+                "mongoSession": {
+                    "enabled": True,
+                    "collection": "userSessions"
+                }
+            },
+            # TODO(aznashwan): check if these are necessary:
+            "routerExemptPaths": [
+                "/editor.worker.js",
+                "/json.worker.js",
+                "/editor.worker.js.map",
+                "/json.worker.js.map",
+                "/version.json",
+                "/config.json",
+                "/favicon.ico",
+                "/static"
+            ],
+            "localAssetPaths": {
+                "/studio/config.json": (
+                    STUDIO_UI_CONFIG_FILE_CONTAINER_LOCAL_PATH)
+            },
+        })
+
+        return None
+
+    def _add_config_file_to_container(
+            self, container: model.Container, container_path: str,
+            config: dict) -> None:
+        """Serializes the provided config dict to JSON and adds it in the
+        Studio service container under the provided path via Pebble API.
+        """
+        LOG.debug(
+            "Adding following config under '%s' in container: %s",
+            container_path, config)
+        container.push(
+            container_path,
+            json.dumps(config),
+            make_dirs=True)
+        LOG.info(
+            "Successfully wrote config file in container under '%s'",
+            container_path)
+
+    def _restart_studio_service(self, container: model.Container) -> None:
+        """Restarts the Studio service using the Pebble container API.
+        """
+        LOG.debug("Restarting Studio service")
+        container.restart("studio")
+        LOG.debug("Successfully issues Studio service restart")
+
+    def _reconfigure_studio_service(self) -> None:
+        """Generates the JSON config for the Studio server and adds it
+        into the container via Pebble files API.
+        - regenerating the JSON config for the Studio server
+        - regenerating the JSON config containing the Engine/SDLC URLs
+        - adding said configs via Pebble
+        - instructing Pebble to restart the Studio server
+        The Studio is power-cycled for the new configuration to take effect.
+        """
+        config = {}
+        possible_blocked_status = (
+            self._add_base_service_config_from_charm_config(config))
+        if possible_blocked_status:
+            LOG.warning("Missing/erroneous configuration options")
+            self.unit.status = possible_blocked_status
+            return
+
+        ui_config = self._get_ui_config_from_relation_data()
+        if not ui_config:
+            self.unit.status = model.BlockedStatus(
+                "Missing relation data for UI config file.")
+            return
+
+        container = self.unit.get_container("studio")
+        with container.can_connect():
+            LOG.debug("Updating Studio service configuration")
+            self._add_config_file_to_container(
+                container,
+                STUDIO_HTTP_CONFIG_FILE_CONTAINER_LOCAL_PATH,
+                config)
+            self._add_config_file_to_container(
+                container,
+                STUDIO_UI_CONFIG_FILE_CONTAINER_LOCAL_PATH,
+                config)
+            self._restart_studio_service(container)
+            self.unit.status = model.ActiveStatus(
+                "Studio service has been started.")
+            return
+
+        LOG.info("Studio container is not active yet. No config to update.")
+        self.unit.status = model.BlockedStatus(
+            "Awaiting Legend DB and Gitlab relations.")
+
+    @_logged_charm_entry_point
+    def _on_config_changed(self, _) -> None:
+        """Reacts to configuration changes to the service by:
+        - regenerating the YAML config for the Studio server
+        - adding it via Pebble
+        - instructing Pebble to restart the Studio server
+        """
+        self._reconfigure_studio_service()
+
+    @_logged_charm_entry_point
+    def _on_db_relation_joined(self, event: charm.RelationJoinedEvent):
+        LOG.debug("No actions are to be performed during DB relation join")
+
+    @_logged_charm_entry_point
+    def _on_db_relation_changed(
+            self, event: charm.RelationChangedEvent) -> None:
+        rel_id = event.relation.id
+        rel = self.framework.model.get_relation("legend-db", rel_id)
+        mongo_creds_json = rel.data[self.app].get("legend-db-connection")
+        if not mongo_creds_json:
+            self.unit.status = model.WaitingStatus(
+                "Awaiting DB relation data.")
+            return
+        LOG.debug(
+            "Mongo JSON credentials returned by DB relation: %s",
+            mongo_creds_json)
+
+        mongo_creds = None
+        try:
+            mongo_creds = json.loads(mongo_creds_json)
+        except (ValueError, TypeError) as ex:
+            LOG.warn(
+                "Exception occured while deserializing DB relation "
+                "connection data: %s", str(ex))
+            self.unit.status = model.BlockedStatus(
+                "Could not deserialize Legend DB connection data.")
+            return
+        LOG.debug(
+            "Deserialized Mongo credentials returned by DB relation: %s",
+            mongo_creds)
+
+        self._stored.mongodb_credentials = mongo_creds
+
+        # Attempt to reconfigure and restart the service with the new data:
+        self._reconfigure_studio_service()
 
 
 if __name__ == "__main__":
-    main(LegendEngineServerOperatorCharm)
+    main.main(LegendStudioServerOperatorCharm)
